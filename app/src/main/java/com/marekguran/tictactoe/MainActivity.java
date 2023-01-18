d14e19429e6cf2fe10f4b44f1e5641d2173a9d69
Marek Guráň <skemgi2@gmail.com>
2023-01-18 17:57:37 +0100

    first commit

---
+++ app/src/main/java/com/marekguran/tictactoe/MainActivity.java
@@ -0,0 +1,192 @@
+package com.marekguran.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import com.marekguran.tictactoe.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    private final List<int[]> combinationList = new ArrayList<>();
+    private int[] boxPositions = {0,0,0,0,0,0,0,0,0}; //9 zero
+    private int playerTurn = 1;
+    private int totalSelectedBoxes = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.playerOneLayout.setBackgroundResource(R.drawable.black_border);
+        binding.playerTwoLayout.setBackgroundResource(R.drawable.white_box);
+        combinationList.add(new int[] {0,1,2});
+        combinationList.add(new int[] {3,4,5});
+        combinationList.add(new int[] {6,7,8});
+        combinationList.add(new int[] {0,3,6});
+        combinationList.add(new int[] {1,4,7});
+        combinationList.add(new int[] {2,5,8});
+        combinationList.add(new int[] {2,4,6});
+        combinationList.add(new int[] {0,4,8});
+        String getPlayerOneName = getIntent().getStringExtra("playerOne");
+        String getPlayerTwoName = getIntent().getStringExtra("playerTwo");
+        binding.playerOneName.setText(getPlayerOneName);
+        binding.playerTwoName.setText(getPlayerTwoName);
+        binding.image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(0)){
+                    performAction((ImageView) view, 0);
+                }
+            }
+        });
+        binding.image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(1)){
+                    performAction((ImageView) view, 1);
+                }
+            }
+        });
+        binding.image3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(2)){
+                    performAction((ImageView) view, 2);
+                }
+            }
+        });
+        binding.image4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(3)){
+                    performAction((ImageView) view, 3);
+                }
+            }
+        });
+        binding.image5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(4)){
+                    performAction((ImageView) view, 4);
+                }
+            }
+        });
+        binding.image6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(5)){
+                    performAction((ImageView) view, 5);
+                }
+            }
+        });
+        binding.image7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(6)){
+                    performAction((ImageView) view, 6);
+                }
+            }
+        });
+        binding.image8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(7)){
+                    performAction((ImageView) view, 7);
+                }
+            }
+        });
+        binding.image9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(8)){
+                    performAction((ImageView) view, 8);
+                }
+            }
+        });
+    }
+    private void performAction(ImageView  imageView, int selectedBoxPosition) {
+        boxPositions[selectedBoxPosition] = playerTurn;
+        if (playerTurn == 1) {
+            imageView.setImageResource(R.drawable.player_one);
+            if (checkResults()) {
+                ResultDialog resultDialog = new ResultDialog(MainActivity.this, binding.playerOneName.getText().toString()
+                        + " " + getString(R.string.winner), MainActivity.this);
+                resultDialog.setCancelable(false);
+                resultDialog.show();
+            } else if(totalSelectedBoxes == 9) {
+                ResultDialog resultDialog = new ResultDialog(MainActivity.this, getString(R.string.draw), MainActivity.this);
+                resultDialog.setCancelable(false);
+                resultDialog.show();
+            } else {
+                changePlayerTurn(2);
+                totalSelectedBoxes++;
+            }
+        } else {
+            imageView.setImageResource(R.drawable.player_two);
+            if (checkResults()) {
+                ResultDialog resultDialog = new ResultDialog(MainActivity.this, binding.playerTwoName.getText().toString()
+                        + " " + getString(R.string.winner), MainActivity.this);
+                resultDialog.setCancelable(false);
+                resultDialog.show();
+            } else if(totalSelectedBoxes == 9) {
+                ResultDialog resultDialog = new ResultDialog(MainActivity.this, getString(R.string.draw), MainActivity.this);
+                resultDialog.setCancelable(false);
+                resultDialog.show();
+            } else {
+                changePlayerTurn(1);
+                totalSelectedBoxes++;
+            }
+        }
+    }
+    private void changePlayerTurn(int currentPlayerTurn) {
+        playerTurn = currentPlayerTurn;
+        if (playerTurn == 1) {
+            binding.playerOneLayout.setBackgroundResource(R.drawable.black_border);
+            binding.playerTwoLayout.setBackgroundResource(R.drawable.white_box);
+        } else {
+            binding.playerTwoLayout.setBackgroundResource(R.drawable.black_border);
+            binding.playerOneLayout.setBackgroundResource(R.drawable.white_box);
+        }
+    }
+    private boolean checkResults(){
+        boolean response = false;
+        for (int i = 0; i < combinationList.size(); i++){
+            final int[] combination = combinationList.get(i);
+            if (boxPositions[combination[0]] == playerTurn && boxPositions[combination[1]] == playerTurn &&
+                    boxPositions[combination[2]] == playerTurn) {
+                response = true;
+            }
+        }
+        return response;
+    }
+    private boolean isBoxSelectable(int boxPosition) {
+        boolean response = false;
+        if (boxPositions[boxPosition] == 0) {
+            response = true;
+        }
+        return response;
+    }
+    public void restartMatch(){
+        boxPositions = new int[] {0,0,0,0,0,0,0,0,0}; //9 zero
+        playerTurn = 1;
+        totalSelectedBoxes = 1;
+        binding.image1.setImageResource(R.drawable.white_box);
+        binding.image2.setImageResource(R.drawable.white_box);
+        binding.image3.setImageResource(R.drawable.white_box);
+        binding.image4.setImageResource(R.drawable.white_box);
+        binding.image5.setImageResource(R.drawable.white_box);
+        binding.image6.setImageResource(R.drawable.white_box);
+        binding.image7.setImageResource(R.drawable.white_box);
+        binding.image8.setImageResource(R.drawable.white_box);
+        binding.image9.setImageResource(R.drawable.white_box);
+    }
+}
---
+++ app/src/main/java/com/marekguran/tictactoe/ResultDialog.java
@@ -0,0 +1,38 @@
+package com.marekguran.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+public class ResultDialog extends Dialog {
+    private final String message;
+    private final MainActivity mainActivity;
+    public ResultDialog(@NonNull Context context, String message, MainActivity mainActivity) {
+        super(context);
+        this.message = message;
+        this.mainActivity = mainActivity;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_dialog);
+        TextView messageText = findViewById(R.id.messageText);
+        Button startAgainButton = findViewById(R.id.startAgainButton);
+        messageText.setText(message);
+        startAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainActivity.restartMatch();
+                dismiss();
+            }
+        });
+    }
+}
